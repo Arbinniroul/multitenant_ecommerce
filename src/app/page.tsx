3d93d46b39be4bddc635b921045c0688089bf535
Arbinniroul <arbinniroula21@gmail.com>
2025-05-05 20:39:40 +0545

    modifiedComponent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-7xl bg-amber-200" >
+    <div className="p-4">
 
-    <Button variant={'cwa'}>Hello world</Button>
+    <div className="flex flex-col gap-4" >
+<div>
+<Button variant={'elevated'}>Im a button</Button>
+</div>
+<div>
+  <Input placeholder="im an input"/>
+</div>
+<div>
+<Progress value={50}/>
+</div>
+<div>
+<Textarea placeholder="im a text area"/>
+</div>
+<div>
+  <Checkbox/>
+</div>
+    </div>
     </div>
   );
 }
