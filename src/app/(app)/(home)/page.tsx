1df649c6a5543e5b7fddc43cdb38d9a0ca3c89e6
Arbinniroul <arbinniroula21@gmail.com>
2025-05-07 22:31:09 +0545

    PayloadAdded

---
+++ src/app/(app)/(home)/page.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
+
+import Image from "next/image";
+
+export default async function Home() {
+    const payload = await getPayload({
+      config: configPromise,
+    })
+    const data = await payload.find({
+      collection:"users",
+    })
+  
+  return (
+   <div>
+    {
+      JSON.stringify(data,null,2)
+    }
+
+   </div>
+  );
+}
